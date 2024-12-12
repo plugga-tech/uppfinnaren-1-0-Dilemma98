38576361097adb99ee1129f18e518ddf0cf9c470
Dilemma98 <emmaah98@outlook.com>
2024-12-12 08:33:02 +0100

    Added index-file with foreach-loop. Not working as planned, need to come back later

--- obj/Debug/net9.0/uppfinnaren-1-0-Dilemma98.AssemblyInfo.cs
+++ obj/Debug/net9.0/uppfinnaren-1-0-Dilemma98.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("uppfinnaren-1-0-Dilemma98")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+039e3bdd475ed40a2fb45dfcde7014fa142e764a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c6fb276b0caf5e06216c3c2d2f1144bc88615b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("uppfinnaren-1-0-Dilemma98")]
 [assembly: System.Reflection.AssemblyTitleAttribute("uppfinnaren-1-0-Dilemma98")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
