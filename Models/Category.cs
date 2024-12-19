ffdc30f78e36acde2c4090a04632d9f1af1f6014
Dilemma98 <emmaah98@outlook.com>
2024-12-19 09:30:50 +0100

    Added Category Class

---
+++ Models/Category.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace uppfinnaren_1_0_Dilemma98.Models;
+
+public class Category
+{
+    public int CategoryId {get; set;}
+    public string Name {get; set;} = string.Empty;
+}
